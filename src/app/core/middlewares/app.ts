a7ecca8b6442c28df3de27417d3e47b6dddec679
naimishverma17 <naimishverma50@gmail.com>
2019-02-18 23:48:49 +0530

    middleware setup

---
+++ src/app/core/middlewares/app.ts
@@ -0,0 +1,30 @@
+import { Store } from '@ngrx/store';
+import { getIsLoading, getIsLoggedIn, State } from '../../central/reducers';
+import { combineLatest, Observable } from 'rxjs';
+import { User } from '../../shared/models/user';
+import { filter, map, take } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppMiddleware {
+  constructor(private store: Store<State>) {
+  }
+
+  register(user: User) {
+    const isLoggedIn$ = this.store.select(getIsLoggedIn);
+    const isLoading$ = this.store.select(getIsLoading);
+    const status$ = combineLatest(isLoading$, isLoggedIn$).pipe(
+      take(1),
+      map(([loading, loggedIn]) => loading || loggedIn,),
+      filter(status => !status));
+    status$.subscribe(status => {
+      // TODO: Api call to register the user on backend!!
+    });
+  }
+
+  getIsLoading(): Observable<boolean> {
+    return this.store.select(getIsLoading);
+  }
+}
