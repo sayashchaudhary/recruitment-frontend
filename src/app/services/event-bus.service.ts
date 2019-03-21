960ca5e82b22846f5ec218a52e4d84c2325a765b
naimishverma17 <naimishverma50@gmail.com>
2019-03-21 19:54:09 +0530

    broadcaster added

---
+++ src/app/services/event-bus.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { IBroadcaster } from '../models/broadcaster';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventBusService {
+
+  private broadcaster: Subject<IBroadcaster>;
+
+  constructor() {
+    this.broadcaster = new Subject<IBroadcaster>();
+  }
+
+  emit(key: string, payload: string) {
+    const event: IBroadcaster = {
+      key, payload
+    };
+    this.broadcaster.next(event);
+  }
+
+  listen(key: string): Observable<string> {
+    return this.broadcaster.asObservable().pipe(
+      filter((event: IBroadcaster) => event.key === key),
+      map((event: IBroadcaster) => event.payload)
+    );
+  }
+}
+
