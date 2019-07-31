5ad7e6631c52482a20db437260018bbc59c5a32f
tanya <tanyaagarwal9722@gmail.com>
2019-07-31 23:26:52 +0530

    changes

--- src/app/middlewares/questions.ts
+++ src/app/middlewares/questions.ts
@@ -25,85 +25,103 @@ import { Constants } from '../utils/constants';
   providedIn: 'root'
 })
 export class QuestionsMiddleware {
-  constructor(private store: Store<RootState>,
-              private httpService: HttpService,
-              private router: Router) {
+  constructor(private s;
 
-  }
+  tore: Store<RootState>
+,
+  private httpService: HttpService
+,
+  private router: Router
+) {
 
-  fetchQuestions() {
-    let isQuestionsLoading$ = this.store.select(getIsQuestionsLoading);
-    let isQuestionsLoaded$ = this.store.select(getIsQuestionsLoaded);
-    combineLatest(isQuestionsLoaded$, isQuestionsLoading$).pipe(
-      take(1),
-      map(([isLoaded, isLoading]) => isLoaded || isLoading),
-      filter(res => !res),
-      switchMap(() => {
-        this.store.dispatch(new FetchQuestions());
-        return this.httpService.get('/questions');
-      }),
-      catchError(err => throwError(err))
-    ).subscribe((res: Question[]) => {
-      if (res && res.length > 0) {
-        this.router.navigate(['instructions']);
-        this.store.dispatch(new AppBootstraped());
-        this.store.dispatch(new FetchQuestionsSuccess(res));
-        this.initializeAnswers(res);
-      } else {
-        this.store.dispatch(new FetchQuestionsFailed());
-      }
-    }, (err) => {
-      console.log('[Error Questions]', err);
+}
+
+fetchQuestions()
+{
+  let isQuestionsLoading$ = this.store.select(getIsQuestionsLoading);
+  let isQuestionsLoaded$ = this.store.select(getIsQuestionsLoaded);
+  combineLatest(isQuestionsLoaded$, isQuestionsLoading$).pipe(
+    take(1),
+    map(([isLoaded, isLoading]) => isLoaded || isLoading),
+    filter(res => !res),
+    switchMap(() => {
+      this.store.dispatch(new FetchQuestions());
+      return this.httpService.get('/questions');
+    }),
+    catchError(err => throwError(err))
+  ).subscribe((res: Question[]) => {
+    if (res && res.length > 0) {
+      this.router.navigate(['instructions']);
+      this.store.dispatch(new AppBootstraped());
+      this.store.dispatch(new FetchQuestionsSuccess(res));
+      this.initializeAnswers(res);
+    } else {
       this.store.dispatch(new FetchQuestionsFailed());
-    });
-  }
-
-  getIsQuestionsLoading() {
-    return this.store.select(getIsQuestionsLoading);
-  }
-
-  getQuestions() {
-    return this.store.select(getQuestions);
-  }
-
-  getAnswers() {
-    return this.store.select(getAnswers);
-  }
-
-  initializeAnswers(questions: Question[]) {
-    const answers: Answer[] = [];
-    const userId = localStorage.getItem(Constants.USER_ID);
-    this.store.select(getIsAnswersInitialized).subscribe(res => {
-      if (!res) {
-        questions.forEach(q => {
-          answers.push({
-            question: q._id,
-            answer: null,
-            user: userId
-          });
-        });
-        this.store.dispatch(new InitializeAnswers(answers));
-      }
-    });
-  }
-
-  updateAnswer(answer: Answer) {
-    if (!answer.question) {
-      return;
     }
-    this.store.dispatch(new UpdateAnswer(answer));
-  }
+  }, (err) => {
+    console.log('[Error Questions]', err);
+    this.store.dispatch(new FetchQuestionsFailed());
+  });
+}
 
-  submitTest() {
-    this.getAnswers().pipe(
-      take(1),
-      switchMap((res: Answer[]) => {
-        console.log('[Answers]', res);
-        return this.httpService.post('/answersupdate', res);
-      })
-    ).subscribe(res => {
-      console.log(res);
-      this.router.navigate(['thankyou'])
-    });
+getIsQuestionsLoading()
+{
+  return this.store.select(getIsQuestionsLoading);
+}
+
+getQuestions()
+{
+  return this.store.select(getQuestions);
+}
+
+getAnswers()
+{
+  return this.store.select(getAnswers);
+}
+
+initializeAnswers(questions
+:
+Question[]
+)
+{
+  const answers: Answer[] = [];
+  const userId = localStorage.getItem(Constants.USER_ID);
+  this.store.select(getIsAnswersInitialized).subscribe(res => {
+    if (!res) {
+      questions.forEach(q => {
+        answers.push({
+          question: q._id,
+          answer: null,
+          user: userId
+        });
+      });
+      this.store.dispatch(new InitializeAnswers(answers));
+    }
+  });
+}
+
+updateAnswer(answer
+:
+Answer
+)
+{
+  if (!answer.question) {
+    return;
   }
+  this.store.dispatch(new UpdateAnswer(answer));
+}
+
+submitTest()
+{
+  this.getAnswers().pipe(
+    take(1),
+    switchMap((res: Answer[]) => {
+      console.log('[Answers]', res);
+      return this.httpService.post('/answersupdate', res);
+    })
+  ).subscribe(res => {
+    console.log(res);
+    this.router.navigate(['thankyou']);
+  });
+},
 }
